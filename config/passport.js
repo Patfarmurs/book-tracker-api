f94280bde93e72c94725309920c5de043964eaf7
unknown <patrickkeita80@yahoo.com>
2025-06-03 13:20:02 -0700

    Add validators.js

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const  passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.use(
+new GoogleStrategy(
+    {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+        // Here you would typically find or create a user in your database
+        // For this example, we'll just return the profile
+        return done(null, profile);
+    }
+)
+);

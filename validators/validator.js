9936dd12a6b4bca3a7b0f20b152a3348bccb2738
unknown <patrickkeita80@yahoo.com>
2025-06-05 04:04:54 -0700

    Update validators.js

--- validators/validator.js
+++ validators/validator.js
@@ -1,50 +1,40 @@
-const { body, validationResul } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
+const userValidationRules = () => [
+    body('username').isEmail().withMessage('Username must be a valid email address'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+];
 
-const userValidationRules = () => {
-    return [
-        body('username').isEmail().withMessage('Username must be a valid email address'),
-        body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-    ]
-}
+const bookValidationRules = () => [
+    body('title').notEmpty().withMessage('Title is required'),
+    body('author').notEmpty().withMessage('Author is required'),
+    body('genre').notEmpty().withMessage('Genre is required'),
+    body('publishedYear').isNumeric().withMessage('Published Year must be a number'),
+    body('ISBN').notEmpty().withMessage('ISBN is required'),
+    body('pages').isNumeric().withMessage('Pages must be a number'),
+    body('status').isIn(['Available', 'Checked out']).withMessage('Status must be either Available or Checked out')
+];
 
-
-
-const bookValidationRules = () => {
-    return [
-        body('title').notEmpty().withMessage('Title is required'),
-        body('author').notEmpty().withMessage('Author is required'),
-        body('genre').notEmpty().withMessage('Genre is required'),
-        body('publishedYear').isNumeric().withMessage('Published Year must be a number'),
-        body('ISBN').notEmpty().withMessage('ISBN is required'),
-        body('pages').isNumeric().withMessage('Pages must be a number'),
-        body('status').isIn(['Available', 'Checked out']).withMessage('Status is required')
-    ]
-}
-
-
-const reviewValidationRules = () => {
-    return [
-        body('bookId').notEmpty().withMessage('Book ID is required'),
-        body('reviewer').notEmpty().withMessage('Reviewer is required'),
-        body('rating').isNumeric().withMessage('Rating must be a number'),
-        body('comment').optional().isString().withMessage('Comment must be a string')
-    ]
-}
-
-
+const reviewValidationRules = () => [
+    body('bookId').notEmpty().withMessage('Book ID is required'),
+    body('reviewer').notEmpty().withMessage('Reviewer is required'),
+    body('rating').isNumeric().withMessage('Rating must be a number'),
+    body('comment').optional().isString().withMessage('Comment must be a string'),
+];
 
 const validate = (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty())
+    if (errors.isEmpty()) {
         return next();
+    }
 
     const extractedErrors = [];
     errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
 
     return res.status(422).json({
-        errors: extractedErrors})
-    };
+        errors: extractedErrors
+    });
+};
 
 module.exports = {
     userValidationRules,
@@ -52,3 +42,4 @@ module.exports = {
     reviewValidationRules,
     validate
 };
+
